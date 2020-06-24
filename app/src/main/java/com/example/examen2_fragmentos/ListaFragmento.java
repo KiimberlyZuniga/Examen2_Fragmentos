723616b0db698a2188c8591a7db6a642fc519b6f
Kim Zúñiga <60992987+KiimberlyZuniga@users.noreply.github.com>
2020-06-23 18:00:30 -0600

    Final Commit KIm

---
+++ app/src/main/java/com/example/examen2_fragmentos/ListaFragmento.java
@@ -0,0 +1,18 @@
+package com.example.examen2_fragmentos;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
+public class ListaFragmento extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.lista_fragmento,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/example/examen2_fragmentos/MainActivity.java
+++ app/src/main/java/com/example/examen2_fragmentos/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.examen2_fragmentos;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spin = (Spinner) findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                ListaFragmento fragmentoSeleccionado = null;
+                switch (pos){
+                    case 0:
+                        fragmentoSeleccionado = new ListaFragmento();
+                        break;
+                    case 1:
+                        fragmentoSeleccionado = new ListaFragmento();
+                }
+
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.fragmento, fragmentoSeleccionado);
+                transaction.commit();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
